06ddc1862b8a016705b542a30976ffe5da2fa562
Forrest Trepte <forrest@microsoft.com>
2021-11-18 11:42:43 -0800

    remove reference to Adder in the docstring
    The user won't necessarily replace docstrings so we want them to read appropriately when the implementation has been replaced with something other than Adder.

--- connector/simulator_model.py
+++ connector/simulator_model.py
@@ -3,8 +3,7 @@ from typing import NamedTuple, Dict, Any
 
 class SimulatorModel:
     """
-    Manages the Adder that represents the simulation for this sample.
-    Implements the reset and step methods required for a Bonsai simulator.
+    Manages the simulation by implementing the reset and step methods required for a Bonsai simulator.
     """
 
     def __init__(self):
